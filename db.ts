9f36ea423a09dc49b7176cab5e8a891cb7721a7c
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-02-18 13:24:47 +0530

    Added database endpoint

---
+++ db.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { PrismaClient } from "./../my-app/lib/database/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
