a7b6feb54e85a505c0c64d52d23d1b4e246aac2f
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-02-19 12:08:40 +0530

    Implemented Better Auth

---
+++ lib/auth-client.ts
@@ -0,0 +1,11 @@
+import { createAuthClient } from "better-auth/client";
+const authClient = createAuthClient();
+
+const signInGoogle = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+  });
+};
+
+export const { signIn, signUp, useSession } = createAuthClient()
+export {signInGoogle};
