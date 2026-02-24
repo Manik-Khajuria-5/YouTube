21b2100ce8b7d6b34b8d2ed61c7d7ab62634a05c
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-02-24 15:45:20 +0530

    Deleted endpoint for a channel created

--- app/api/channel/[id]/route.ts
+++ app/api/channel/[id]/route.ts
@@ -2,13 +2,16 @@ import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { success } from "zod";
 
-export async function GET(request : NextRequest){
+export async function GET(request : NextRequest,{params}:{
+    params : Promise<{id : string}>
+}){
    
-    const userId = request.headers.get("x-user-id")!;
+    const resolvedParams = await params;
+    const userId = resolvedParams.id;
 
     try{
        
-        const response = await prisma.channel.findMany({
+        const response = await prisma.channel.findUnique({
             where : {
                 userId : userId
             }
@@ -17,6 +20,40 @@ export async function GET(request : NextRequest){
         return NextResponse.json({
             success : true,
             message : "Here are the users channel",
+            Channels : response
+        },{
+            status : 200
+        })
+    }
+    catch(err){
+
+        return NextResponse.json({
+           message : "Internal Server Error",
+           err : "err"
+        },{
+            status : 500
+        })
+    }
+};
+
+export async function DELETE(request : NextRequest,{params}:{
+    params : Promise<{id : string}>
+}){
+   
+    const resolvedParams = await params;
+    const userId = resolvedParams.id;
+
+    try{
+       
+        const response = await prisma.channel.delete({
+            where : {
+                userId : userId
+            }
+        })
+
+        return NextResponse.json({
+            success : true,
+            message : "User channel deleted successfully",
             Channels : response
         },{
             status : 200
