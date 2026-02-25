af8af84021f1e29ee79beb4d3e5447c81605fb72
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-02-25 14:11:42 +0530

    Added delete endpoint for like

--- app/api/like/[id]/route.ts
+++ app/api/like/[id]/route.ts
@@ -90,3 +90,54 @@ export async function GET(
     );
   }
 }
+
+//endpoint for delete likes for a channel
+export async function DELETE(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  },
+) {
+  const { id } = await params;
+
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  const userId = session!.session.userId;
+
+  try {
+    const response = await prisma.likes.delete({
+      where: {
+        uploadId_userId: {
+          uploadId : id,
+          userId: userId,
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "UnLiked successfully",
+        SubscribeCnt: response,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+        error: "err",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
