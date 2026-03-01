78a3985f2529debc96ccdb4c83b009076376dd2f
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-03-01 15:49:45 +0530

    added delete endpoint

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -1,4 +1,6 @@
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/db";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request : NextResponse,{params}:{
@@ -30,6 +32,55 @@ export async function GET(request : NextResponse,{params}:{
           comments : response
        },{
         status : 201
+       })
+    }
+    catch(err){
+        return NextResponse.json({
+           message : "Internal Server Error",
+           err : "err"
+        },{
+            status : 500
+        });
+    }
+}
+
+export async function DELETE(request : NextResponse,{params}:{
+    params : Promise<{id : String}>
+}){
+
+    const session = await auth.api.getSession({
+        headers : request.headers
+    });
+      
+    const userId = session!.session.userId;
+
+
+    const {id} = await params;
+    
+    if(!id || typeof id !== "string"){
+       
+        return NextResponse.json({
+           message : "Invalid Schema",
+           err : "err"
+        },{
+            status : 403
+        })
+    }
+    
+    try{
+       
+       const response = await prisma.comments.delete({
+         where : {
+            userId : userId,
+            id : id,
+         }
+       })
+
+       return NextResponse.json({
+          message : "Comment deleted successfully",
+          comments : response
+       },{
+        status : 203
        })
     }
     catch(err){
