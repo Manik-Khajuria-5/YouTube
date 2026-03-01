00b6776a6bf31ec8c36bc052f3087e1d3ff32790
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-03-01 12:25:21 +0530

    Added random commit

---
+++ app/api/watch-history/[id]/route.ts
@@ -0,0 +1,47 @@
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/db";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { success } from "zod";
+
+//adding watch history for an uopload id
+export async function POST(request : NextRequest,{params}:{
+    params : Promise<{id : string}>
+}){
+
+    const session = await auth.api.getSession({
+        headers : request.headers
+    });
+
+    const userId = session!.session.userId;
+    const {id} = await params;
+
+    try{
+      
+       const response = await prisma.watchHistory.create({
+        data : {
+            uploadId : id,
+            userId : userId
+        }
+       });
+
+       return NextResponse.json({
+          success : true,
+          message : "Watch History Created Sucessfully",
+          WatchId : response.id
+       },{
+        status : 201
+       });
+
+    }
+    catch{
+      
+        return NextResponse.json({
+            success : false,
+            message : "Internal Server Error",
+            err : "err"
+        },{
+            status : 500
+        });
+    }
+}
---
+++ database/prisma/migrations/20260225092048_manik2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "watchHistory" ALTER COLUMN "watchedAt" DROP NOT NULL;
